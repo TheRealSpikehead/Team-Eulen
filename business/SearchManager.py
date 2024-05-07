cb5e6bb640ed2f45cab3acf834fd0813a32cec80
JanSteiner34 <jan.steiner1@students.fhnw.ch>
2024-05-07 21:14:58 +0200

    gel_all_rooms get_available_hotels 1.1.4 (muss noch bearbeitet werden) get_hotel_informations 1.1.5

--- business/SearchManager.py
+++ business/SearchManager.py
@@ -1,11 +1,16 @@
 from pathlib import Path
+import sqlite3
 
 import sm
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, join, Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import joinedload, session
+from sqlalchemy import and_, or_, not_
+from sqlalchemy import func
 from sqlalchemy.orm import sessionmaker, scoped_session
 from data_access.data_base import init_db
 from data_models.models import *
+from datetime import date
+from data_models.models import Booking
 
 
 class SearchManager(object):
@@ -34,27 +39,72 @@ class SearchManager(object):
         allhotels = self.__session.execute(query).scalars().all()
         return allhotels
 
+    def get_all_rooms_by_date(self, date: str):
+        pass
+
+    def get_all_rooms(self):
+        query = select(Room)
+        rooms = self.__session.execute(query).scalars().all()
+        return rooms
+
+    def get_available_hotels(self, city: str, start_date: date, end_date: date, max_guests: int):  # 1.1.4
+        query = select(Hotel.name, Hotel.stars, Room.number, Room.max_guests). \
+            join(Address). \
+            outerjoin(Room, Hotel.id == Room.hotel_id). \
+            outerjoin(Booking, and_(Room.number == Booking.id,
+                                    or_(Booking.end_date < start_date, Booking.start_date > end_date))). \
+            where(and_(Address.city == city, Room.max_guests >= max_guests)). \
+            group_by(Hotel.id, Room.number). \
+            having(func.count(Booking.id) == 0)
+
+        available_rooms = self.__session.execute(query).fetchall()
+        return available_rooms
+
+    def get_hotel_informations(self):  # 1.1.5
+        j = join(Hotel, Address, Hotel.address_id == Address.id)
+        query = select(Hotel.name, Hotel.stars, Address.street, Address.zip, Address.city).select_from(j)
+        details = self.__session.execute(query)
+        return details
+
 
 if __name__ == "__main__":
+
     sm = SearchManager('../data/database.db')
 
-    name = input("Geben Sie den Namen des Hotels ein (oder lassen Sie das Feld leer): ")
-    stars_input = input("Geben Sie die Sterneanzahl des Hotels ein (oder lassen Sie das Feld leer): ")
-    address = input("Geben Sie die Stadtadresse des Hotels ein (oder lassen Sie das Feld leer): ")
+    # name = input("Geben Sie den Namen des Hotels ein (oder lassen Sie das Feld leer): ")
+    # stars_input = input("Geben Sie die Sterneanzahl des Hotels ein (oder lassen Sie das Feld leer): ")
+    # address = input("Geben Sie die Stadtadresse des Hotels ein (oder lassen Sie das Feld leer): ")
+    #
+    # # Konvertierung der Sterne-Eingabe in einen Integer, falls nicht leer
+    # stars = int(stars_input) if stars_input.isdigit() else None
+    #
+    # # Suche durchführen
+    # hotels = sm.get_hotels(name=name if name else None, stars=stars, address=address if address else None)
+    #
+    # if hotels:
+    #     print(f"Anzahl gefundener Hotels: {len(hotels)}")
+    #     for hotel in hotels:
+    #         print(hotel)  # Angenommen, `hotel` ist eine Instanz, die sinnvoll als String dargestellt werden kann
+    # else:
+    #     print("Keine Hotels gefunden, die den Kriterien entsprechen.")
 
-    # Konvertierung der Sterne-Eingabe in einen Integer, falls nicht leer
-    stars = int(stars_input) if stars_input.isdigit() else None
+    # hotels = sm.get_hotels()
+    # for hotel in hotels:
+    #     print(hotel)
 
-    # Suche durchführen
-    hotels = sm.get_hotels(name=name if name else None, stars=stars, address=address if address else None)
+    # allrooms = sm.get_all_rooms()
+    # for room in allrooms:
+    #     print(room)
 
-    if hotels:
-        print(f"Anzahl gefundener Hotels: {len(hotels)}")
-        for hotel in hotels:
-            print(hotel)  # Angenommen, `hotel` ist eine Instanz, die sinnvoll als String dargestellt werden kann
-    else:
-        print("Keine Hotels gefunden, die den Kriterien entsprechen.")
+    # details = sm.get_hotel_informations()  ## 1.1.5
+    # for detail in details:
+    #     print(f"Name: {detail[0]}, Stars: {detail[1]}, Street: {detail[2]}, Zip: {detail[3]}, City: {detail[4]}")
 
+    start_date = date(2024, 1, 1)
+    end_date = date(2024, 12, 31)
+    city = "Olten"
+    max_guests = 2
 
-
-
+    available_hotels = sm.get_available_hotels(city, start_date, end_date, max_guests)
+    for hotel in available_hotels:
+        print(f"Hotel: {hotel[0]}, Stars: {hotel[1]}, Room: {hotel[2]}, Capacity: {hotel[3]}")
