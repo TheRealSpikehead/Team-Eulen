cf4fe67107881ffd7b4b2a7efd33cfe1a15c75a6
Robin <robin.meier@students.fhnw.ch>
2024-05-09 15:26:50 +0200

    Weiter Funktionalitäten für den NavigationManager

--- business/NavigationManager.py
+++ business/NavigationManager.py
@@ -1,44 +1,48 @@
-1# include all navigation functions here
+# include all navigation functions here
 # create menu, display menu, hand-over tasks to different "Managers" e.g. SearchManager/UserManager etc.
 
 from console_base import *
+from SearchManager import SearchManager
+
+# Annahme: Der Dateipfad zur Datenbankdatei wird als Argument übergeben
+database_file = "../data/database.db"
+
+# Erstelle eine Instanz von SearchManager und übergebe den Dateipfad zur Datenbankdatei
+search_manager = SearchManager(database_file)
+
+# Rufe die Methode get_all_hotels auf der Instanz auf
+all_hotels = search_manager.get_all_hotels()
 
 class AllHotels(Menu):
-    def __init__(self):
+    def __init__(self, back):
         super().__init__("HomeScreen - Guest")
-        self.add_option(MenuOption("View all Hotels"))
-        self.add_option(MenuOption("Filter Hotels"))
+        for hotel in all_hotels:
+            self.add_option(MenuOption(hotel))
+#        start_index = 0
+#       self.add_option(MenuOption(all_hotels[0]))
+#       self.add_option(MenuOption(all_hotels[1]))
+#        self.add_option(MenuOption(all_hotels[start_index:0]))
         self.add_option(MenuOption("Quit"))
+        self._back = back
 
     def _navigate(self, choice: int):
-        match choice:
-            case 1:
-                from SearchManager import SearchManager
-
-                # Annahme: Der Dateipfad zur Datenbankdatei wird als Argument übergeben
-                database_file = "../data/database.db"
-
-                # Erstelle eine Instanz von SearchManager und übergebe den Dateipfad zur Datenbankdatei
-                search_manager = SearchManager(database_file)
-
-                # Rufe die Methode get_all_hotels auf der Instanz auf
-                all_hotels = search_manager.get_all_hotels()
-
-                # Drucke die erhaltenen Hotels aus
-                for hotel in all_hotels:
-                    print(hotel)
-                return None
-            case 2:
-                return None
-            case 3:
-                return None
+        if choice == len(all_hotels) + 1:
+            # Benutzer hat "Back" ausgewählt
+            return self._back
+        elif 1 <= choice <= len(all_hotels):
+            # Der Benutzer hat ein Hotel ausgewählt
+            return all_hotels[choice - 1]
+        else:
+            print("Ungültige Auswahl.")
+            return None
 class HomeScreenGuest(Menu):
-    def __init__(self):
+    def __init__(self, back):
         super().__init__("HomeScreen - Guest")
         self.add_option(MenuOption("View all Hotels"))
-        self._All_Hotels = AllHotels()
+        self._All_Hotels = AllHotels(self)
         self.add_option(MenuOption("Filter Hotels"))
         self.add_option(MenuOption("Quit"))
+        self._back = back
 
     def _navigate(self, choice: int):
         match choice:
@@ -47,13 +51,13 @@ class HomeScreenGuest(Menu):
             case 2:
                 return None
             case 3:
-                return None
+                return self._back
 
 class HotelMenu(Menu):
     def __init__(self, back):
         super().__init__("Hotel Management")
         self.add_option(MenuOption("Login as Guest"))
-        self._Home_Guest = HomeScreenGuest()
+        self._Home_Guest = HomeScreenGuest(self)
         self.add_option(MenuOption("Login with Account"))
         self.add_option(MenuOption("Register New Account"))
         self.add_option(MenuOption("Quit"))
@@ -84,7 +88,6 @@ class MainMenu(Menu):
         super().__init__("Main Menu")
         self.add_option(MenuOption("Hotel Management"))
         self._hotel_menu = HotelMenu(self)
-
         self.add_option(MenuOption("Quit"))
 
     def _navigate(self, choice: int):
